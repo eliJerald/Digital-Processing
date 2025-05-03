faf4eb002c6eb34efdc4923a8f0d4b7e6add5f60
Eli Rosales <ejerald.cr@gmail.com>
2025-05-02 22:23:57 -0700

    Update download_dataset.py

--- backend/download_dataset.py
+++ backend/download_dataset.py
@@ -4,7 +4,10 @@ Eli Jerald C Rosales
 Downloads the dataset from Kaggle using the kagglehub library.
 This script is used to download the dataset from Kaggle using the kagglehub library.
 
+# pip install kagglehub[pandas-datasets]
+
 Citation:
+
 BibTeX
 @misc{make ml,
 title={Car License Plates Dataset},
@@ -20,12 +23,11 @@ Photo by Gunnar Bjarki on Unsplash
 
 # kagglehub: a library that allows you to download datasets from Kaggle.
 import kagglehub
-# pip install kagglehub[pandas-datasets]
 
 # Download latest version
 path = kagglehub.dataset_download("andrewmvd/car-plate-detection")
 # windows path example:
 # C:\Users\<username>\.cache\kagglehub\datasets\andrewmvd\car-plate-detection\versions\1\images
-# C:\Users\<username>eli\.cache\kagglehub\datasets\andrewmvd\car-plate-detection\versions\1\annotations
+# C:\Users\<username>\.cache\kagglehub\datasets\andrewmvd\car-plate-detection\versions\1\annotations
 
 print("Path to dataset files:", path)
